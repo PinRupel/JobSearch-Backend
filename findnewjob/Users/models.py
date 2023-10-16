76346d3515447d3bae483e6e7b41c5a5f4284a65
Renata <ilovevdm@gmail.com>
2023-10-16 19:48:27 +0700

    Созданы модели пользователей

--- findnewjob/Users/models.py
+++ findnewjob/Users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .manager import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('Email'), unique=True)
+    is_applicant = models.BooleanField(default=False)
+    is_employer = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Applicant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='applicant')
+    first_name = models.CharField(max_length=150, default="Name")
+    last_name = models.CharField(max_length=150, default="Last name")
+    email = models.EmailField(unique=True)
+
+
+class Employer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='employer')
+    company_name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(unique=True)
+
